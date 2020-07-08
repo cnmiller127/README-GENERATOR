5dacf89362122e79722eeb730d9b741f815a9a67
Christian Miller <cnmiller127@gmail.com>
2020-07-08 18:59:04 -0500

    added data

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# title 
- desc
+# Date generator 
+ 
+    ## Description 
+
+    lines up dates 
+
+    ## Installation 
+
+    they install it duh 
+
+    ## Usage 
+
+    they operate it duh 
+
+    ## Contribution 
+
+    none bro 
+
+    ## Tests 
+
+    none bro 
+
+    ## Questions 
+
+    ### E-mail
+    cnmiller127@gmail.com
+    ### Github
+    [My Github Profile](https://github.com/cnmiller127)
+    
+    
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var inquirer = require("inquirer");
 var fs = require('fs');
 const { title } = require("process");
+const Choices = require("inquirer/lib/objects/choices");
 // array of questions for user
 const questions = [
     {
@@ -11,8 +12,45 @@ const questions = [
     {
         type: "input",
         name: "description",
-        message: "Describe your project "
-    }
+        message: "Describe your project: "
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "How does the user install this application? "
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "How does the user operate this application? "
+},
+
+{
+  type: "list",
+  name: "license",
+  message: "How would you like to license this username? ",
+  choices: ["By username", "By repository name"]
+},
+{
+  type: "input",
+  name: "contribution",
+  message: "What limitations would you like to put on the contributions of other developers? "
+},
+{
+  type: "input",
+  name: "test",
+  message: "How would should this application be tested? "
+},
+{
+  type: "input",
+  name: "email",
+  message: "Enter your e-mail: "
+},
+{
+  type: "input",
+  name: "github",
+  message: "Enter your github user-name: "
+},
 
 ];
 
@@ -27,10 +65,31 @@ function init() {
 inquirer.prompt(questions).then(function(data) {
 
   var filename = "README.md";
-  console.log(data.title)
-  console.log(data.description)
+  console.log(data)
+  const {title, description, installation, usage, license, contribution, test, email, github} = data;
 
-  fs.writeFile(filename, `# ${data.title} \n ${data.description}`, function(err) {
+  fs.writeFile(filename, 
+    `# ${title} \n 
+    ## Description \n
+    ${description} \n
+    ## Installation \n
+    ${installation} \n
+    ## Usage \n
+    ${usage} \n
+    ## Contribution \n
+    ${contribution} \n
+    ## Tests \n
+    ${contribution} \n
+    ## Questions \n
+    ### E-mail
+    ${email}
+    ### Github
+    [My Github Profile](https://github.com/${github})
+    
+    `, 
+    
+    
+    function(err) {
 
     if (err) {
       return console.log(err);
